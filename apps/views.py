4d73fbbd4736e77619cf794f8b84fcf499021a4d
morfat <morfatmosoti@gmail.com>
2017-10-25 03:26:13 +0300

    apps

--- apps/views.py
+++ apps/views.py
@@ -5,10 +5,10 @@ from .models import App
 class List(object):
     def on_get(self,req,resp):
         resp.status=falcon.HTTP_200 #this is default
-        #apps=App(self.db).all()
+        apps=App(self.db).all()
 
-        app=App(self.db).one(pk=1)
-        resp.media=app
+        #app=App(self.db).one(pk=1)
+        resp.media=apps
 
     
         
